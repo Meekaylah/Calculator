2dc98489b7e649e329155f77c0b451aba625b5ef
Meekaylah <84780375+Meekaylah@users.noreply.github.com>
2023-03-03 13:55:10 +0100

    Made basic functions work

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-import ast as ast
 
 calculation = ""
+
 
 def add_to_calculation(symbol):
     global calculation
@@ -9,59 +9,67 @@ def add_to_calculation(symbol):
     text_result.delete(1.0, "end")
     text_result.insert(1.0, calculation)
 
+
 def evaluate_calculation():
     global calculation
     try:
-        tree = ast.parse(calculation, mode='eval')
-    except SyntaxError:
-        return  # not a Python expression
-    if not all(isinstance(node, (ast.Expression,
-                                 ast.UnaryOp, ast.unaryop,
-                                 ast.BinOp, ast.operator,
-                                 ast.Num)) for node in ast.walk(tree)):
-        return  # not a mathematical expression (numbers and operators)
-
-    try:
-        result = eval(compile(tree, filename='', mode='eval'))
-        calculation = ""
+        calculation = str(eval(calculation))
         text_result.delete(1.0, "end")
-        text_result.insert(1.0, result)
+        text_result.insert(1.0, calculation)
     except:
         clear_field()
         text_result.insert(1.0, "Error")
-        pass
+
 
 def clear_field():
     global calculation
     calculation = ""
-    text_result  .delete(1.0, "end")
+    text_result.delete(1.0, "end")
     pass
 
 
 root = tk.Tk()
-root.geometry("275x325")
+root.geometry("191x270")
 
-text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
-text_result.grid(columnspan=4)
+text_result = tk.Text(root, height=1, width=8, font=("Arial", 32))
+text_result.grid(columnspan=5)
 
-btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), width=5, )
-btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1))
-btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2))
-btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3))
-btn_4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4))
-btn_5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5))
-btn_6 = tk.Button(root, text="6", command=lambda: add_to_calculation(6))
-btn_7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7))
-btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8))
-btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9))
-btn_AC = tk.Button(root, text="AC", command=lambda: clear_field())
-btn_plus_or_minus = tk.Button(root, text="⁺/₋", command="")
-btn_percent = tk.Button(root, text="%", command="")
-btn_divide = tk.Button(root, text="÷", command="")
-btn_multiply = tk.Button(root, text="×", command="")
-btn_subtract = tk.Button(root, text="-", command="")
-btn_add = tk.Button(root, text="+", command="")
-btn_decimal = tk.Button(root, text=".", command="")
-btn_equal = tk.Button(root, text="=", command="")
+btn_0 = tk.Button(root, text="0", command=lambda: add_to_calculation(0), height=1, width=3, font=("Arial", 16))
+btn_0.grid(row=5, column=1, columnspan=2, sticky="nsew")
+btn_1 = tk.Button(root, text="1", command=lambda: add_to_calculation(1), height=1, width=3, font=("Arial", 16))
+btn_1.grid(row=4, column=1, sticky="nsew")
+btn_2 = tk.Button(root, text="2", command=lambda: add_to_calculation(2), height=1, width=3, font=("Arial", 16))
+btn_2.grid(row=4, column=2, sticky="nsew")
+btn_3 = tk.Button(root, text="3", command=lambda: add_to_calculation(3), height=1, width=3, font=("Arial", 16))
+btn_3.grid(row=4, column=3, sticky="nsew")
+btn_4 = tk.Button(root, text="4", command=lambda: add_to_calculation(4), height=1, width=3, font=("Arial", 16))
+btn_4.grid(row=3, column=1, sticky="nsew")
+btn_5 = tk.Button(root, text="5", command=lambda: add_to_calculation(5), height=1, width=3, font=("Arial", 16))
+btn_5.grid(row=3, column=2, sticky="nsew")
+btn_6 = tk.Button(root, text="6", command=lambda: add_to_calculation(6), height=1, width=3, font=("Arial", 16))
+btn_6.grid(row=3, column=3, sticky="nsew")
+btn_7 = tk.Button(root, text="7", command=lambda: add_to_calculation(7), height=1, width=3, font=("Arial", 16))
+btn_7.grid(row=2, column=1, sticky="nsew")
+btn_8 = tk.Button(root, text="8", command=lambda: add_to_calculation(8), height=1, width=3, font=("Arial", 16))
+btn_8.grid(row=2, column=2, sticky="nsew")
+btn_9 = tk.Button(root, text="9", command=lambda: add_to_calculation(9), height=1, width=3, font=("Arial", 16))
+btn_9.grid(row=2, column=3, sticky="nsew")
+btn_AC = tk.Button(root, text="AC", command=clear_field, height=1, width=3, font=("Arial", 16))
+btn_AC.grid(row=1, column=1, sticky="nsew")
+btn_plus_or_minus = tk.Button(root, text="⁺/₋", command="", height=1, width=3, font=("Arial", 16))
+btn_plus_or_minus.grid(row=1, column=2, sticky="nsew")
+btn_percent = tk.Button(root, text="%", command=lambda: add_to_calculation("%"), height=1, width=3, font=("Arial", 16))
+btn_percent.grid(row=1, column=3, sticky="nsew")
+btn_divide = tk.Button(root, text="÷", command=lambda: add_to_calculation("/"), height=1, width=3, font=("Arial", 16))
+btn_divide.grid(row=1, column=4, sticky="nsew")
+btn_multiply = tk.Button(root, text="×", command=lambda: add_to_calculation("*"), height=1, width=3, font=("Arial", 16))
+btn_multiply.grid(row=2, column=4, sticky="nsew")
+btn_subtract = tk.Button(root, text="-", command=lambda: add_to_calculation("-"), height=1, width=3, font=("Arial", 16))
+btn_subtract.grid(row=3, column=4, sticky="nsew")
+btn_add = tk.Button(root, text="+", command=lambda: add_to_calculation("+"), height=1, width=3, font=("Arial", 16))
+btn_add.grid(row=4, column=4, sticky="nsew")
+btn_decimal = tk.Button(root, text=".", command=lambda: add_to_calculation("."), height=1, width=3, font=("Arial", 16))
+btn_decimal.grid(row=5, column=3, sticky="nsew")
+btn_equal = tk.Button(root, text="=", command=evaluate_calculation, height=1, width=3, font=("Arial", 16))
+btn_equal.grid(row=5, column=4, sticky="nsew")
 root.mainloop()
-
